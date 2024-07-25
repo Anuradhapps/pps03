916186df2b7dda7f1d048450eb4e557e16582f85
Anuradha_pps <anuradha.pps2024@gmail.com>
2024-07-25 16:17:22 +0530

    done

--- resources/views/pest_data/create.blade.php
+++ resources/views/pest_data/create.blade.php
@@ -46,7 +46,7 @@
                             </div>
                         @endforeach
 
-                        <div class="mb-4">
+                        <div class="mb-4 ">
                             <label for="other_pest_incident" class="block text-sm font-medium text-gray-700">Other Pest Incident</label>
                             <input type="text" name="other_pest_incident" id="other_pest_incident" placeholder="Other Pest Incident" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">
                         </div>
